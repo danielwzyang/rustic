63e82473e886ae8c011d37e2bc0a119a7a3a2ddf
Daniel Yang <danielwzyang@gmail.com>
2025-12-05 21:47:00 -0500

    iterate in lexer with regex captures instead of whitespace split

--- src/interpreter/lexer.rs
+++ src/interpreter/lexer.rs
@@ -6,65 +6,50 @@ use std::{
 use regex::Regex;
 use super::{tokens::{Token, TokenType}, read_lines};
 
-// Regex patterns for different token types
-
 pub fn tokenize(path: &str, keywords: HashMap<&str, TokenType>) -> Result<VecDeque<Token>, Box<dyn Error>> {
+    let token_regex = Regex::new(r"(?x)
+        (?P<WhiteSpace> \s+) |
+        (?P<Number> -?(\d+\.?\d*|\.\d+)([eE][+-]?\d+)?) |
+        (?P<FilePath>[A-Za-z0-9_]+\.[A-Za-z0-9]+) |
+        (?P<Identifier> [a-zA-Z_][a-zA-Z0-9_]*) |
+        (?P<Unknown> \S)"
+    ).unwrap();
+
     let mut tokens: VecDeque<Token> = VecDeque::new();
+    let lines = read_lines(path).map_err(|_| format!("Script '{}' not found", path))?; 
 
-    let number_regex = Regex::new(r"^-?(\d+\.?\d*|\.\d+)([eE][+-]?\d+)?$").unwrap();
-    let identifier_regex = Regex::new(r"^[a-zA-Z_][a-zA-Z0-9_]*$").unwrap();
-    let file_path_regex = Regex::new(r"^(\.{0,2}/)?([a-zA-Z0-9_\-./]*[a-zA-Z0-9_\-])?\.([a-zA-Z0-9]+)$").unwrap();
-
-    let lines = read_lines(path).map_err(|_| format!("Script '{}' not found", path))?;
-
-    let mut iterator = lines.map_while(Result::ok).enumerate();
-
-    while let Some((line_number, line)) = iterator.next() {
+    for (line_number, line) in lines.map_while(Result::ok).enumerate() {
         let line = line.trim();
 
-        // ignore comments
         if line.starts_with("//") || line.starts_with("#") || line.is_empty() {
             continue;
         }
 
-        // convert line to token strings
-        let current = line.split_whitespace().collect::<Vec<&str>>();
-
-        for token in current {
-            // keyword
-            if let Some(token_type) = keywords.get(token) {
-                tokens.push_back(Token { 
-                    value: token.to_string(), 
-                    token_type: token_type.clone() 
-                });
-            } 
-            
-            // number
-            else if number_regex.is_match(token) {
+        for captures in token_regex.captures_iter(line) {
+            if captures.name("WhiteSpace").is_some() {
+                continue;
+            } else if let Some(number) = captures.name("Number") {
                 tokens.push_back(Token {
-                    value: token.to_string(),
+                    value: number.as_str().to_string(),
                     token_type: TokenType::Number,
                 });
-            } 
-            
-            // file path
-            else if file_path_regex.is_match(token) {
+            } else if let Some(file_path) = captures.name("FilePath") {
                 tokens.push_back(Token {
-                    value: token.to_string(),
+                    value: file_path.as_str().to_string(),
                     token_type: TokenType::FilePath,
                 });
-            } 
-            
-            // identifier
-            else if identifier_regex.is_match(token) {
+            } else if let Some(identifier) = captures.name("Identifier") {
+                let identifier = identifier.as_str();
+
+                let token_type = keywords.get(identifier).cloned().unwrap_or(TokenType::Identifier);
+                
                 tokens.push_back(Token {
-                    value: token.to_string(),
-                    token_type: TokenType::Identifier,
+                    value: identifier.to_string(),
+                    token_type,
                 });
-            } 
-            
-            else {
-                return Err(format!("{}:{} Token not recognized: {}", path, line_number + 1, token).into());
+            } else if let Some(unknown) = captures.name("Unknown") {
+                println!("{:?}", tokens);
+                return Err(format!("{}:{} Token not recognized: {}", path, line_number + 1, unknown.as_str()).into());
             }
         }
     }
